106395bda147541dfedbff6899faa3aeea55dada
NihalBambale <nihalbambale2000@gmail.com>
2026-06-20 21:22:48 +0530

    Security fix: Remove personal backups and local build folders

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/appSaveVault_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appSaveVault")
-set(working_dir "E:/SaveVault")
-set(src_and_dest_list
-
-)
-set(timestamp_file "E:/SaveVault/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/appSaveVault_res.txt")
--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/appSaveVault.dir/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/rcc/qrc_qmake_SaveVault.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/appSaveVault.dir/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/rcc/qrc_qmake_SaveVault.cpp.obj: \
- E:\SaveVault\build\Desktop_Qt_6_11_0_MinGW_64_bit-Debug\.qt\rcc\qrc_qmake_SaveVault.cpp
